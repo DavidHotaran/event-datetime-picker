45c6f1609e7e1d4b7b1331f5e7ed07954b780bd6
DavidHotaran <hotaran.d98@gmail.com>
2022-09-23 21:53:41 -0400

    added dotenv config and db config

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const events= require('./routes/eventRoute');
+const connectDB = require('./config/db');
 
-app.get('/', (req, res) => {
-    res.json({"message": "hello world"});
-});
+require('dotenv').config();
+connectDB();
 
-app.listen(PORT, () => console.log(`App started on port ${PORT}`))
+app.use(express.json());
+app.use('/api/event', events);
+
+app.listen(PORT, () => console.log(`App started on port ${PORT}`));
