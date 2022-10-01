8860c7e118ccac4a18da1f41e1fb67bfc42664f8
DavidHotaran <hotaran.d98@gmail.com>
2022-10-01 16:13:57 -0400

    added CORS

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,12 @@ const PORT = 5000;
 const events= require('./routes/eventRoute');
 const user= require('./routes/userRoute');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 require('dotenv').config();
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/event', events);
 app.use('/api/user', user);
