dafea589bdb5cf01ada658d29ec373a6411b7674
DavidHotaran <hotaran.d98@gmail.com>
2022-10-01 11:14:09 -0400

    added user route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 const events= require('./routes/eventRoute');
+const user= require('./routes/userRoute');
 const connectDB = require('./config/db');
 
 require('dotenv').config();
@@ -9,5 +10,6 @@ connectDB();
 
 app.use(express.json());
 app.use('/api/event', events);
+app.use('/api/user', user);
 
 app.listen(PORT, () => console.log(`App started on port ${PORT}`));
